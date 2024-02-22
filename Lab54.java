84a19a7f286d18d35b400c0a0c3ef4be6a7f0773
KalpViradia <160616083+KalpViradia@users.noreply.github.com>
2024-02-22 22:44:43 +0530

    Update Lab54.java

--- Lab54.java
+++ Lab54.java
@@ -1,21 +1,31 @@
-import Java.util.Scanner;
+import java.util.Scanner;
 public class Lab54{
 	public static void main(String[] args) {
-		
+		Bank b1 = new Bank(56789,200000);
+		b1.getaccountDetails();
+		b1.printaccountDetails();	
 	}
 }
 
-class Bank_Account{
+class Bank{
 	int accountNo;
-	string userName;
-	string email;
-	string accountType;
-	double accountBalance;
-
-	void getAccountDetails(){
-				
+	int accountbalance;
+	Bank(int  accountNo,int accountbalance){
+		this.accountNo = accountNo;
+		this.accountbalance=accountbalance;
 	}
-	void displayAccountDetails(){
 
+	void getaccountDetails(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter accountNo:");
+		this.accountNo = sc.nextInt();
+		System.out.println("enter accountbalance:");
+		this.accountbalance = sc.nextInt();
+	}
+
+
+	void printaccountDetails(){
+		System.out.println("accountNo: "+this.accountNo);
+		System.out.println("accountbalance : "+this.accountbalance);
 	}
 }
